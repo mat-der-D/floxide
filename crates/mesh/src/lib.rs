91296e89a9b944eabe487e541114e25c10c5ef25
Smooth Pudding <dummy@address>
2026-03-03 13:20:04 +0900

    [feat] Implement tasks

--- crates/mesh/src/lib.rs
+++ crates/mesh/src/lib.rs
@@ -4,7 +4,18 @@
 
 mod error;
 mod geometry;
+mod global_mesh_data;
+mod patch;
+mod poly_mesh;
 mod primitive_mesh;
+mod zone;
 
 pub use error::MeshError;
+pub use global_mesh_data::GlobalMeshData;
+pub use patch::{
+    CoupledPatch, CyclicPolyPatch, EmptyPolyPatch, PatchKind, PatchSpec, PolyPatch,
+    ProcessorPolyPatch, SymmetryPolyPatch, Transform, WallPolyPatch, WedgePolyPatch,
+};
+pub use poly_mesh::PolyMesh;
 pub use primitive_mesh::PrimitiveMesh;
+pub use zone::{FaceZone, Zone};
